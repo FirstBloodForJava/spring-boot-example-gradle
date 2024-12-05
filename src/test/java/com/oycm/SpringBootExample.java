0369a2cfd9a39377ebe3aa48f7eba8b2aa2638d6
ouyangcm <mingorg@163.com>
2024-12-05 17:06:45 +0800

    初始化简单调整的项目

---
+++ src/test/java/com/oycm/SpringBootExample.java
@@ -0,0 +1,13 @@
+package com.oycm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExample {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
